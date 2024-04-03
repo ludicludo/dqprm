7de982f9111a80c4e74d1845930507e9ee45fe92
Ferrer Ludovic <ludovic.ferrer@ico.unicancer.fr>
2024-04-03 11:06:10 +0200

    meilleur design avec ajout __name__

--- extract_suv.py
+++ extract_suv.py
@@ -2,10 +2,16 @@ import re
 
 fic = "20200527152905.861802.ig.tum"
 
-with open(fic, "r") as f:
-    lines = f.read()
 
-m = re.findall("(M.*)(?<!RC)SUVValue = (\d+\.\d+)", lines, re.M)
+def do_it():
+    with open(fic, "r") as f:
+        lines = f.read()
 
-d = {el[0]: float(el[1]) for el in m}
-print(d)
+    m = re.findall("(M.*)(?<!RC)SUVValue = (\d+\.\d+)", lines, re.M)
+
+    d = {el[0]: float(el[1]) for el in m}
+    print(d)
+
+
+if __name__ == "__main__":
+    do_it()
